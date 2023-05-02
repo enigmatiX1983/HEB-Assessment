0ad6ab25406bb8fb1ba576464056f4785a6e6880
Robert M Martin <robertm83@gmail.com>
2023-05-01 22:23:50 -0500

    Adding models for Items and Coupons
    - Added Item.java
    - Added Coupon.java

---
+++ src/main/java/com/heb/assessment/model/Coupon.java
@@ -0,0 +1,31 @@
+package com.heb.assessment.model;
+
+public class Coupon {
+    String couponName;
+    long appliedSku;
+    float discountPrice;
+
+    public String getCouponName() {
+        return couponName;
+    }
+
+    public void setCouponName(String couponName) {
+        this.couponName = couponName;
+    }
+
+    public long getAppliedSku() {
+        return appliedSku;
+    }
+
+    public void setAppliedSku(long appliedSku) {
+        this.appliedSku = appliedSku;
+    }
+
+    public float getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(float discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+}
---
+++ src/main/java/com/heb/assessment/model/Item.java
@@ -0,0 +1,49 @@
+package com.heb.assessment.model;
+
+public class Item {
+    private String itemName;
+    private long sku;
+    private boolean isTaxable;
+    private boolean ownBrand;
+    private float price;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public long getSku() {
+        return sku;
+    }
+
+    public void setSku(long sku) {
+        this.sku = sku;
+    }
+
+    public boolean isTaxable() {
+        return isTaxable;
+    }
+
+    public void setTaxable(boolean taxable) {
+        isTaxable = taxable;
+    }
+
+    public boolean isOwnBrand() {
+        return ownBrand;
+    }
+
+    public void setOwnBrand(boolean ownBrand) {
+        this.ownBrand = ownBrand;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
