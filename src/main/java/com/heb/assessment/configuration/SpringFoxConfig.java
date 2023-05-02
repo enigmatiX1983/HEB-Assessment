d34958295a50d5b7e3ba5ff8bb7d0bb7248244bd
Robert M Martin <robertm83@gmail.com>
2023-05-01 21:49:15 -0500

    Configuration changes for Swagger UI
    - Adding POM entries for Swagger UI

--- src/main/java/com/heb/assessment/configuration/SpringFoxConfig.java
+++ src/main/java/com/heb/assessment/configuration/SpringFoxConfig.java
@@ -2,20 +2,22 @@ package com.heb.assessment.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 public class SpringFoxConfig {
     @Bean
-    public Docket productApi() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.todo"))
-                .paths(regex("/todo.*"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 }
