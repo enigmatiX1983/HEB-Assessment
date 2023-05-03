953280eb3abd597ad30af4a4edf05f811f2fc52c
Robert M Martin <robertm83@gmail.com>
2023-05-03 00:36:02 -0500

    Update SpringFoxConfig.java
    - Specify package for controller to remove basic-error-controller

--- src/main/java/com/heb/assessment/configuration/SpringFoxConfig.java
+++ src/main/java/com/heb/assessment/configuration/SpringFoxConfig.java
@@ -6,9 +6,6 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 public class SpringFoxConfig {
@@ -16,7 +13,7 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.any())
+            .apis(RequestHandlerSelectors.basePackage("com.heb.assessment.controller"))
             .paths(PathSelectors.any())
             .build();
     }
